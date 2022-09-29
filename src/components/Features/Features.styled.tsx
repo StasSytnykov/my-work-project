13800d95cac11f435142ac30ebaefc9daba52a6a
Stas Sytnykov <stas82kak05@gmail.com>
2022-09-29 13:26:30 +0300

    added adaprive version to features section

--- src/components/Features/Features.styled.tsx
+++ src/components/Features/Features.styled.tsx
@@ -1,40 +1,59 @@
 import styled from "styled-components";
 
 const FeaturesSection = styled.section`
-  margin-bottom: 150px;
+  margin-bottom: 50px;
+
+  @media screen and (min-width: 1200px) {
+    margin-bottom: 150px;
+  }
 `;
 
 const FeaturesTextThumb = styled.div`
-  display: flex;
+  @media screen and (min-width: 1200px) {
+    display: flex;
+  }
 `;
 
 const FeaturesP = styled.p`
   font-weight: 500;
-  font-size: 16px;
+  font-size: 14px;
   line-height: 1.4;
   text-transform: uppercase;
   color: #03a8b7;
 
-  margin-right: 346px;
+  @media screen and (min-width: 1200px) {
+    font-size: 16px;
+    margin-right: 346px;
+  }
 `;
 
 const FeaturesThumb = styled.div`
-  width: 570px;
+  @media screen and (min-width: 1200px) {
+    width: 570px;
+  }
 `;
 
 const FeaturesTitle = styled.h1`
   font-weight: 600;
-  font-size: 46px;
+  font-size: 32px;
   line-height: 1.19;
   color: #202731;
-  margin-bottom: 24px;
+  margin-bottom: 15px;
+  @media screen and (min-width: 1200px) {
+    font-size: 46px;
+    margin-bottom: 24px;
+  }
 `;
 
 const FeaturesDesc = styled.p`
   font-size: 16px;
   line-height: 1.4;
   color: rgba(32, 39, 49, 0.75);
-  margin-bottom: 49px;
+  margin-bottom: 30px;
+
+  @media screen and (min-width: 1200px) {
+    margin-bottom: 49px;
+  }
 `;
 
 export {
--- src/components/Features/FeaturesList/FeaturesList.styled.tsx
+++ src/components/Features/FeaturesList/FeaturesList.styled.tsx
@@ -5,18 +5,41 @@ interface IFeaturesText {
 }
 
 const FeaturesListStyled = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media screen and (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 30px;
+    place-items: center;
+  }
+
+  @media screen and (min-width: 1200px) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 60px;
+  }
 `;
 
 const FeaturesItemStyled = styled.li`
   position: relative;
-  width: 360px;
+  width: 340px;
   height: 318px;
   border: 1px solid rgba(0, 0, 0, 0.1);
   box-shadow: 0 15px 30px rgba(0, 0, 0, 0.05);
   border-radius: 15px;
+
+  @media screen and (max-width: 767px) {
+    &:not(:last-child) {
+      margin-bottom: 20px;
+    }
+  }
+
+  @media screen and (min-width: 1200px) {
+    width: 360px;
+  }
 `;
 
 const FeaturesListContentThumb = styled.div`
